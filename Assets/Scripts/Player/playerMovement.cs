ed9f3635f6375b074705214f28339990fde84c3c
Jose Galvez <a01272733@itesm.mx>
2017-04-21 13:40:13 -0500

    Procedural level generation

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -7,17 +7,23 @@ public class Boundary
 {
     public float xMin, xMax, yMin, yMax;
 }
+
 public class playerMovement : MonoBehaviour {
 
-    public Boundary boundary;
+	public Boundary boundary;
 
+	//Velocidad de la nave.
+	public float rotationSpeed;
+	public float translationSpeed;
 
-    public float speed;
-    public float tilt;
     public GameObject shot;
     public Transform shotSpawn;
     public float fireRate;
     public float shipSensitive;
+
+    Vector3 InitialDevicePosition;
+    Vector3 movement;
+    public float smooth = 0.5f;
 
     private float nextFire;
 
@@ -26,6 +32,7 @@ public class playerMovement : MonoBehaviour {
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        InitialDevicePosition = Input.acceleration;
     }
 
 
@@ -41,30 +48,13 @@ public class playerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate () {
-        float xAce = Input.acceleration.x;
-        float yAce = Input.acceleration.y;
-        float zAce = Input.acceleration.z;
-        Vector3 movement;
+		//Mover la nave hacia adelante
+		rb.velocity = transform.forward * translationSpeed;
 
+		//Rotar la nave segun el input del acelerometro.
+		movement = Vector3.Lerp(movement, Input.acceleration - InitialDevicePosition, smooth);
 
-        //movement = new Vector3(-Input.GetAxis("Horizontal"), -Input.GetAxis("Vertical"), 1f);
-        //Debug.Log(" X: " + Input.GetAxis("Horizontal") + " Y: " + Input.GetAxis("Vertical"));
+		transform.Rotate (movement.y * rotationSpeed, -movement.x * rotationSpeed, 0.0f);
 
-        movement = new Vector3(-xAce, -yAce, 1f);
-        rb.velocity = movement * speed;
-
-        rb.position = new Vector3
-            (
-               Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
-               Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax),
-               rb.position.z
-            );
-
-        if(Mathf.Abs(xAce) > shipSensitive || Mathf.Abs(yAce) > shipSensitive)
-            rb.rotation = Quaternion.Euler(rb.velocity.y * -tilt, rb.velocity.x * tilt, 0.0f);
-        else
-        {
-            rb.rotation = Quaternion.Euler(0f, 0f, 0.0f);
-        }
     }
 }
